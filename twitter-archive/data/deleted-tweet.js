02f3c20a3f4a8bedc38b777b4c0cec74658e8f9d
Your Name <you@example.com>
2022-09-13 18:07:03 +0200

    ...

---
+++ twitter-archive/data/deleted-tweet.js
@@ -0,0 +1,22 @@
+window.YTD.deleted_tweet.part0 = [
+  {
+    "tweet" : {
+      "text" : "Liebes @MInkorrekt ODER @ReinhardRemfort ODER @nicolas_woehrl",
+      "user_id" : "1410298424910561285",
+      "id" : "1565226472956170242",
+      "media" : [ ],
+      "created_at" : "Thu Sep 01 06:34:06 +0000 2022",
+      "deleted_at" : "Thu Sep 01 06:34:17 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "text" : "Mal etwas Positives:\nich hatte grad das Glück mobilen ähm obdachlosen 30 jährigen christlichen Wanderprediger kennen lernen zu dürfen\n\nEr hat schön auf einen Punkt gebracht was ich schon lange tue\n\n\"Man muss bei Entscheidungen immer die für einen unbequemere Entscheidung treffen\"",
+      "user_id" : "1410298424910561285",
+      "id" : "1566868411975704577",
+      "media" : [ ],
+      "created_at" : "Mon Sep 05 19:18:35 +0000 2022",
+      "deleted_at" : "Mon Sep 05 19:21:13 +0000 2022"
+    }
+  }
+]
