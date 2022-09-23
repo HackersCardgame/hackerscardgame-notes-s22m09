da3030f6b184437bda8c958f740e01480e34e114
Your Name <you@example.com>
2022-09-23 02:53:59 +0200

    ...

---
+++ twitter/data/deleted-tweet.js
@@ -0,0 +1,36 @@
+window.YTD.deleted_tweet.part0 = [
+  {
+    "tweet" : {
+      "text" : "RT @c_videogames: Diesen Lemmings Level kannte ich noch gar nicht! 🤔\n\n🤪 https://t.co/fyyxkkdTFP",
+      "user_id" : "1410298424910561285",
+      "share" : {
+        "source_status_id" : "1538774184314167298",
+        "source_user_id" : "179454469",
+        "parent_status_id" : "1538774184314167298",
+        "passthrough_fields" : { }
+      },
+      "id" : "1538837117421355010",
+      "media" : [ ],
+      "created_at" : "Mon Jun 20 10:52:13 +0000 2022",
+      "deleted_at" : "Mon Sep 19 16:01:51 +0000 2022"
+    }
+  },
+  {
+    "tweet" : {
+      "text" : "Dann Info-germaktik Vorlesung:\n\n@xkcd erklärt Malware welche Turing-Completenes ner Grafik Library ausnutzt...deshalb haben die das so lang nicht gefunden...\n\nhttps://t.co/fYFmLjCCOI\n\nhttps://securityboulevard. com/2021/12/nso-zero-click-exploit-turing-complete-cpu-in-image-file/ https://t.co/1AMUuv1u3v",
+      "user_id" : "1410298424910561285",
+      "id" : "1572976399987638273",
+      "media" : [
+        {
+          "passthrough_fields" : { },
+          "height" : "-1",
+          "id" : "1572975733902872578",
+          "media_type" : "1",
+          "width" : "-1"
+        }
+      ],
+      "created_at" : "Thu Sep 22 15:49:32 +0000 2022",
+      "deleted_at" : "Thu Sep 22 15:51:29 +0000 2022"
+    }
+  }
+]
